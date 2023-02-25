ca41fc95cefd4f57992762e7fdc0c76c7c21e843
Daniel Craigie <danielcraigie@me.com>
2023-02-25 23:49:42 +0000

    Removed overcomplicated O-O approach to DynamoDB integration for a starter project.

---
+++ .bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+
+use Dotenv\Dotenv;
+use Aws\Sdk;
+
+require implode(DIRECTORY_SEPARATOR, ['vendor', 'autoload.php']);
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+/*
+ * Init AWS PHP SDK
+ */
+$sdk = new Sdk([
+    'credentials' => [
+        'key' => $_ENV['AWS_ACCESS_KEY_ID'],
+        'secret' => $_ENV['AWS_SECRET_ACCESS_KEY'],
+    ],
+    'region' => $_ENV['REGION'],
+    'version' => 'latest',
+]);
+
+$dynClient = $sdk->createDynamoDb([
+    'version' => $_ENV['DYNAMODB_VERSION'],
+    'endpoint' => $_ENV['DYNAMODB_ENDPOINT'],
+]);
