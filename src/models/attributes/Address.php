f5a80f3201b282dac88a47e8b1184919b45b6242
Daniel Craigie <danielcraigie@me.com>
2023-03-06 09:30:48 +0000

    Refactored AWS PHP SDK connections out of bootstrap into static Class. Refactored table name variable passing into $_ENV calls.

--- src/models/attributes/Address.php
+++ src/models/attributes/Address.php
@@ -3,6 +3,7 @@
 namespace Danielcraigie\Bookshop\models\attributes;
 
 use Aws\Result;
+use Danielcraigie\Bookshop\AWS\AWS;
 use Exception;
 
 class Address extends AbstractAttribute
@@ -15,17 +16,17 @@ class Address extends AbstractAttribute
      */
     public function create(string $partitionKey, mixed $value):void
     {
-        $result = $this->getDynamoDbClient()->putItem([
+        $result = AWS::DynamoDB()->putItem([
             'Item' => [
                 'PK' => ['S' => $partitionKey],
                 'SK' => ['S' => $this->getPartitionKey()],
                 'Details' => ['S' => json_encode($value)],
             ],
-            'TableName' => $this->getTableName(),
+            'TableName' => $_ENV['TABLE_NAME'],
         ]);
 
         if (!$result instanceof Result) {
-            throw new Exception(sprintf('Could not add Address for \"%s\" to [%s].',$partitionKey, $this->getTableName()));
+            throw new Exception(sprintf('Could not add Address for \"%s\" to [%s].',$partitionKey, $_ENV['TABLE_NAME']));
         }
     }
 }
